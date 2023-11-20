631c0640138c9bdafac14adb2bf5aab84968aa9c
Doit <duhee7524@naver.com>
2023-11-21 07:44:20 +0900

    component 생성

---
+++ react-app2/react-app2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Toast from './components/Toast';
+
+function App() {
+  return (
+    <div className="Wrapper">
+      <Toast/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app2/react-app2/src/components/Toast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Toast.css";
+
+const Toast = () => {
+    return (
+        <div className = 'wrapper'>
+            <div className='toast'>
+                <main className='toast__message'> 
+                    <p className='toast__message-title'>dddd</p>
+                    <p className='toast__message-text'>aaaa</p>
+                </main>
+                <button className='toast__button'>ss</button>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Toast;
