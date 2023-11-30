b457046361cece754032db43e46dcf2224db61bd
Doit <duhee7524@naver.com>
2023-11-30 11:21:39 +0900

    CSS-in-JS 방식의 스타일링

---
+++ react-styling3/my-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {styled} from "styled-components"
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] =useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if(event.target.value.trim().length >0){
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setEnteredValue("");
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <FormControl >
+        <FormControlLabel>목표</FormControlLabel>
+        <FormControlInput type="text" value={enteredValue} onChange={goalInputChangeHandler} isValid={isValid} />
+      </FormControl>
+      <Button type="submit">목표 추가하기</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+`;
+
+const FormControlLabel= styled.label`
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+`;
+const FormControlInput =styled.input`
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+  ${props=> !props.isValid && `
+    background-color: red;
+    border-color: red;
+  `}
+`;
