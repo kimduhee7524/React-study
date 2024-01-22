b62d5698ceaea46a9d3dd95d04d623ddd941892e
Doit <duhee7524@naver.com>
2024-01-22 20:49:04 +0900

    Redux Toolkit 사용하기2
    여러개의 Slice 활용하기
    Counter 이외에 Auth state 까지 같이 하나의 Store에서 관리하기

---
+++ react-redux3/my-app/src/components/Header.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import classes from './Header.module.css';
+import { authActions } from '../store';
+
+const Header = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  }
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+              <nav>
+              <ul>
+                <li>
+                  <a href='/'>내 상품들</a>
+                </li>
+                <li>
+                  <button onClick={logoutHandler}>로그아웃</button>
+                </li>
+              </ul>
+            </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
